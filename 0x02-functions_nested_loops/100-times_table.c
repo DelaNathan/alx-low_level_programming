07f14603292d024c0ba8f8f4ea85edcdc268a93f
DelaNathan <delali35@yahoo.com>
2023-02-22 01:34:58 -0800

    100 times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,45 +1,49 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: integer value
- *
- * Return: void
+ * print_times_table - Print the `n` times table, starting with 0.
+ * Description: If `n` is greater than 15 or less than 0, print nothing.
+ * @n: int type number
  */
 void print_times_table(int n)
 {
-	int i, j, res;
+	int x = 0, y, z;
 
-	if (n >= 0 && n <= 15)
+	if (n > 15 || n < 0)
+		return;
+	while (x <= n)
 	{
-		for (i = 0; i <= n; i++)
+		for (y = 0; y <= n; y++)
 		{
-			_putchar('0');
-			for (j = 1; j <= n; j++)
+			z = x * y;
+			if (z > 99)
+			{
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (z > 9)
+			{
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+				_putchar(z + '0');
+
+			if (y != n)
 			{
 				_putchar(',');
 				_putchar(' ');
-				res = i * j;
-				if (res < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(res + '0');
-				}
-				else if (res < 100)
-				{
-					_putchar(' ');
-					_putchar(res / 10 + '0');
-					_putchar(res % 10 + '0');
-				}
-				else
-				{
-					_putchar(res / 100 + '0');
-					_putchar((res / 10) % 10 + '0');
-					_putchar(res % 10 + '0');
-				}
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
+		x++;
 	}
 }
