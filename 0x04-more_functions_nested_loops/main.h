cee3fc7af87c26e5bfbfb5d28465859b89fc9f7e
DelaNathan <delali35@yahoo.com>
2023-02-23 08:21:07 -0800

    triangle

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,6 +4,12 @@
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int add(int, int);
+int b10_pow(int idx);
+int _abs(int);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
@@ -13,5 +19,8 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+
 
 #endif
