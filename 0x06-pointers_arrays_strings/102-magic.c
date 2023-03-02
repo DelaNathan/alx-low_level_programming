9ff0f456fb36ddcf8662a563dc6c9747a5958671
DelaNathan <delali35@yahoo.com>
2023-03-01 18:29:03 -0800

    magic

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,21 @@
+#include <stdio.>
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	/*
+	 * write your linr of code here..
+	 * Remember:
+	 * - you are not allowed to use a
+	 *   - you are not allowed to modify p
+	 *   - only one statement
+	 *   - you are not allowed to code anything else than tjis line of code
+	 */
+printf("a[2] = %d\n", a[2]);
+return (0);
+}
