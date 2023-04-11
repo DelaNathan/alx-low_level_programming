a8b2c3a3966c7b0b07e29ad3aee3abfcf6abc064
DelaNathan <delali35@yahoo.com>
2023-04-10 21:07:17 -0700

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 		for (m = 0; text_content[m];)
 			m++;
 	}
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0700);
 	w = write(o, text_content, m);
 
 	if (o == -1 || w == -1)
