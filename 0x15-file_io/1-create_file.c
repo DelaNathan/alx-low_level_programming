57082b7ec6429339d0c76e593d096b85b1bf8fe8
DelaNathan <delali35@yahoo.com>
2023-04-10 20:46:26 -0700

    snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * creat_file - creates a file
+ * @filename: points to name of file to create
+ * @text_content: pointer to string to write to file
+ * Return: -1 if function fails
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int o;
+	int w;
+	int m = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (m = 0; text_content[m];)
+			m++;
+	}
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC);
+	w = write(o, text_content, m);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
+}
