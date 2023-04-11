0dbb5beb8812e8708328c715997624fc739df194
DelaNathan <delali35@yahoo.com>
2023-04-10 18:30:44 -0700

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,17 +3,17 @@
 
 /**
  * print_binary - a function that prints the binary representation of a number
- * @n: integer
+ * @n: long integer
  * Return: Always (0)
  **/
 void print_binary(unsigned long int n)
 {
-	if ((n > 1) == 0 && n == 0)
+	if ((n >> 1) == 0 && n == 0)
 	{
 		_putchar('0');
 		return;
 	}
 	if (n >> 1 != 0)
-		print_binary(n > 1);
+		print_binary(n >> 1);
 	_putchar((n & 1) + '0');
 }
