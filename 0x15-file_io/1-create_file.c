fea72f25b5e1944ecbb8ab4f444acd0637e99915
DelaNathan <delali35@yahoo.com>
2023-04-10 21:44:45 -0700

    snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (o == -1)
 		return (-1);
 	if (text_content)
