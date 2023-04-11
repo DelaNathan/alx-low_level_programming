f0a8bb01dee76a82a60ce9cd0ae4b114314c072c
DelaNathan <delali35@yahoo.com>
2023-04-10 18:34:50 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,22 +9,22 @@
  **/
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result, m;
+	unsigned int i, m;
 
 	m = 0;
-	result = 0;
+	i = 0;
 	if (!b)
 		return (0);
 
-	while (b[result] != '\0')
+	while (b[i] != '\0')
 
 	{
-		if (b[result] != '0' && b[result] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		m <<= 1;
-		if (b[result] & 1)
+		if (b[i] & 1)
 			m += 1;
-		result += 1;
+		i += 1;
 	}
 	return (m);
 }
